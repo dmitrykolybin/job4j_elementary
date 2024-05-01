92849f2c037418f5322c4073a4b7374df7588dc9
dmitrykolybin <kolybin.dv@gmail.com>
2024-05-01 15:18:05 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitry Kolybin");
+        System.out.println("05.09.2003");
+    }
+}
