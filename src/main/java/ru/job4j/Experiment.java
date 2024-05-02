4ea83fcbb7aa1d0b5effcc41cfc2b29214bd02d9
dmitrykolybin <kolybin.dv@gmail.com>
2024-05-02 13:16:48 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/Experiment.java
+++ src/main/java/ru/job4j/Experiment.java
@@ -3,9 +3,7 @@ package ru.job4j;
 public class Experiment {
 
     public static void main(String[] args) {
-        double starDistance = 4.7E+12;
-        double bacteriaNumber = 2.0E+7;
-        System.out.println(starDistance);
-        System.out.println(bacteriaNumber);
+        int result = (3 + 3) * 3;
+        System.out.println(result);
     }
 }
