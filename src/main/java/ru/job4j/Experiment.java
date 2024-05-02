de6c9f4dcf4b415240329302552d61e169763075
dmitrykolybin <kolybin.dv@gmail.com>
2024-05-02 12:42:08 +0300

    4.3. Вызов метода без указания класса [#504945]

---
+++ src/main/java/ru/job4j/Experiment.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Experiment {
+
+    public static void main(String[] args) {
+        double starDistance = 4.7E+12;
+        double bacteriaNumber = 2.0E+7;
+        System.out.println(starDistance);
+        System.out.println(bacteriaNumber);
+    }
+}
