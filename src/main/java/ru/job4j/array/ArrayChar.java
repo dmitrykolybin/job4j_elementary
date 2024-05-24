59cb4dbab1ad1bbc62fd465d83aef94f14b6854f
dmitrykolybin <kolybin.dv@gmail.com>
2024-05-24 23:01:07 +0300

    some changes

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,7 +1,6 @@
 package ru.job4j.array;
 
 public class ArrayChar {
-
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
         for (int index = 0; index < prefix.length; index++) {
--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -1,7 +1,6 @@
 package ru.job4j.array;
 
 public class SortSelected {
-
     public static int[] sort(int[] data) {
         for (int start = 0; start < data.length; start++) {
             int min = MinDiapason.findMin(data, start, data.length - 1);
